a9f7ef65e32aa2b77e6b19afbec6675c7813b50d
rubanik <anton.rubanik@gmail.com>
2021-08-20 21:33:16 +0300

    commited

--- magic_test.py
+++ magic_test.py
@@ -1,14 +1,20 @@
+import datetime
+
 class MagicTest():
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self,name) -> None:
+        self.log = 'log'
+        self.name = name
 
     def __str__(self) -> str:
-        return 'Hey you use the __str__ magic'
+        return 'Hey, you use the __str__ magic'
 
+    def __setattr__(self, name, value):
+        print(f"{datetime.datetime.now()} : {name} was changed. The New name is {value}")
 
 if __name__ == '__main__':
-
-    magic = MagicTest()
-
-    print(magic)
+    magic = MagicTest('Ui')
+    #print(magic)
+    magic.name = 'Io'
+    magic.name = 'Lui'
+    
